0ff7d7ca543ebe7bb8ff599304c80588840fb839
athiragi <athirasnair72476@gmail.com>
2022-05-19 04:17:15 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Employee from './component/Employee';
+import Search from './component/Search';
 
 function App() {
   return (
     <div >
       <Employee/>
+      <Search/>
     
     </div>
   );
